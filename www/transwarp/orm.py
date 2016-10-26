a4ca9deaf2e998d4c0f7ca022f67ce63c086afee
Alex Wang <alexwang09@hotmail.com>
2016-10-26 13:59:44 +0800

    change author's name

--- www/transwarp/orm.py
+++ www/transwarp/orm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = 'Alex Wang'
+__author__ = 'Michael Liao'
 
 '''
 Database operation module. This module is independent with web module.
--- www/transwarp/web.py
+++ www/transwarp/web.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = 'Alex Wang'
+__author__ = 'Michael Liao'
 
 '''
 A simple, lightweight, WSGI-compatible web framework.
