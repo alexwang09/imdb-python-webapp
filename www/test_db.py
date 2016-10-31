dcac061a9a6fe2ec0f6cf06f010f47f6b6344b75
Alex Wang <alexwang09@hotmail.com>
2016-10-31 17:42:15 +0800

    add Python files of test_db and urls

--- www/test_db.py
+++ www/test_db.py
@@ -8,8 +8,21 @@ from models import User, Movie, Review
 from transwarp import db
 
 if __name__ == '__main__':
-    print Movie().__sql__()
+    #print Movie().__sql__()
     db.create_engine('root', 'wxy6772102', 'imdb')
+    #id = '0014778151211044fbebfc019244892b8714a9ce9553d3e000'
+    name = '大圣归来'
+    summary = '五百年前，由石猴变化而成的齐天大圣孙悟空（张磊 配音）大闹天宫，最终被如来佛祖镇在了五行山下。此去经年，长安城内突然遭到山妖洗劫，童男童女哭声连连，命悬一线。危机时刻，自幼被行脚僧法明（吴文伦 配音）抚养长大的江流儿（林子杰 配音）救下了一个小女孩，结果反遭山妖追杀。经过一番追逐，江流儿无意中解除了孙悟空的封印，齐天大圣自然好好地将山妖教训了一番。因为封印未解开，悟空不得不护送江流儿和小女孩回长安，一路上又遭遇了猪八戒（刘九容 配音）和白龙马。'
+    duration = 89
+    style = '喜剧 动作 动画 奇幻'
+    date = '20150710'
+    director = '田晓鹏'
+    writer = '田晓鹏 刘虎 米粒 金冉 金成'
+    star = '张磊 林子杰 吴文伦 童自荣 刘九容 吴迪 刘北辰 赵乾景 周帅'
+
+    m = Movie(name=name, summary=summary, duration=duration, style=style, date=date, director=director, writer=writer, star=star)
+    m.insert()
+    '''
     db.update('drop table if exists users')
     db.update('drop table if exists movies')
     db.update('drop table if exists reviews')
@@ -18,7 +31,7 @@ if __name__ == '__main__':
     db.update(Review().__sql__())
     u = User(name='Test', email='test@example.com', password='1234567890')
     u.insert()
-    '''
+
     u = User(name='Test', email='test@example.com', password='1234567890', image='about:blank')
 
     u.insert()
--- www/urls.py
+++ www/urls.py
@@ -16,6 +16,9 @@ from config import configs
 _COOKIE_NAME = 'imdbsession'
 _COOKIE_KEY = configs.session.secret
 
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_MD5 = re.compile(r'^[0-9a-f]{32}$')
+
 def _get_page_index():
     page_index = 1
     try:
@@ -79,8 +82,112 @@ def manage_interceptor(next):
         return next()
     raise seeother('/signin')
 
-@view('movies.html')
+@view('index.html')
 @get('/')
 def index():
     movies, page = _get_movies_by_page()
+    for movie in movies:
+        movie.year = movie.date[0:4]
+        movie.directors = movie.director.split(' ')
+        movie.writers = movie.writer.split(' ')
+        movie.stars = movie.star.split(' ')
+        movie.styles = movie.style.split(' ')
+        movie.summarys = movie.summary.split("\r")
+        movie.num_directors = len(movie.directors)
+        movie.num_writers = len(movie.writers)
+        movie.num_stars = len(movie.stars)
+        movie.num_styles = len(movie.styles)
     return dict(page=page, movies=movies, user=ctx.request.user)
+
+@view('register.html')
+@get('/register')
+def register():
+    return dict()
+
+@view('signin.html')
+@get('/signin')
+def signin():
+    return dict()
+
+@get('/signout')
+def signout():
+    ctx.response.delete_cookie(_COOKIE_NAME)
+    raise seeother('/')
+
+@get('/manage/')
+def manage_index():
+    raise seeother('/manage/reviews')
+
+@view('manage_review_list.html')
+@get('/manage/reviews')
+def manage_reviews():
+    return dict(page_index=_get_page_index(), user=ctx.request.user)
+
+@api
+@post('/api/users')
+def register_user():
+    i = ctx.request.input(name='', email='', password='')
+    name = i.name.strip()
+    email = i.email.strip().lower()
+    password = i.password
+    if not name:
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not password or not _RE_MD5.match(password):
+        raise APIValueError('password')
+    user = User.find_first('where name=?', name)
+    if user:
+        raise APIError('register:failed', 'name', 'Name is already in use.')
+    user = User.find_first('where email=?', email)
+    if user:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+    user = User(name=name, email=email, password=password, image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email).hexdigest())
+    user.insert()
+    # make session cookie:
+    cookie = make_signed_cookie(user.id, user.password, None)
+    ctx.response.set_cookie(_COOKIE_NAME, cookie)
+    return user
+
+@api
+@post('/api/name_authenticate')
+def name_authenticate():
+    i = ctx.request.input(name='')
+    name = i.name.strip()
+    if not name:
+        raise APIValueError('name')
+    user = User.find_first('where name=?', name)
+    if user:
+        raise APIError('register:failed', 'name', 'Name is already in use.')
+    return dict()
+
+@api
+@post('/api/email_authenticate')
+def email_authenticate():
+    i = ctx.request.input(email='')
+    email = i.email.strip().lower()
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    user = User.find_first('where email=?', email)
+    if user:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+    return dict()
+
+@api
+@post('/api/authenticate')
+def authenticate():
+    i = ctx.request.input(remember='')
+    email = i.email.strip().lower()
+    password = i.password
+    remember = i.remember
+    user = User.find_first('where email=?', email)
+    if user is None:
+        raise APIError('auth:failed', 'email', 'Invalid email.')
+    elif user.password != password:
+        raise APIError('auth:failed', 'password', 'Invalid password.')
+    # make session cookie:
+    max_age = 604800 if remember=='true' else None
+    cookie = make_signed_cookie(user.id, user.password, max_age)
+    ctx.response.set_cookie(_COOKIE_NAME, cookie, max_age=max_age)
+    user.password = '******'
+    return user
